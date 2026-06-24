42e457995804a16cc9f83303d3600c65e21ab544
hungbasic <k.hung090804@gmail.com>
2026-06-24 19:46:18 +0700

    chore: khởi tạo dự án fintech

---
+++ backend-saas/src/main/java/com/fintech/smartwealth/SmartWealthApplication.java
@@ -0,0 +1,13 @@
+package com.fintech.smartwealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartWealthApplication.class, args);
+	}
+
+}
---
+++ backend-saas/src/test/java/com/fintech/smartwealth/SmartWealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.smartwealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartWealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
